2793ceba4952106d9e4654520fcccde9b3985c26
Murali Krishna Madala <murali.madala@qentelli.com>
2022-01-27 11:51:56 +0530

    nodeapi

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // Importing expressjs
 
+//express js foer
 const express = require('express')
+const bodyParser = require('body-parser')
+const {request} = require('express')
 const app =express()
+app.use(bodyParser.urlencoded({extended : true}))
+app.post('/quotes',(req,res) =>{
+    res.send(req.body)
+})
 
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.sendFile(__dirname + './index.html')
 })
 
 const PORT=3000
